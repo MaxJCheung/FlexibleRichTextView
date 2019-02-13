88b9c3a7dc8498c68b14b683a8c163ef12ba8ed1
jiancheung <zhj@mbadashi.com>
2019-02-13 13:53:08 +0800

    add text size and text color attr

--- library/src/main/java/com/daquexian/flexiblerichtextview/FlexibleRichTextView.java
+++ library/src/main/java/com/daquexian/flexiblerichtextview/FlexibleRichTextView.java
@@ -7,7 +7,9 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.text.Layout;
 import android.text.Spanned;
@@ -35,8 +37,10 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
 
 import java.util.ArrayList;
@@ -498,18 +502,15 @@ public class FlexibleRichTextView extends LinearLayout {
         final int finalHeight = imgHeight;
         Glide.with(mContext)
                 .load(url)
-                .placeholder(new ColorDrawable(ContextCompat.getColor(mContext, android.R.color.darker_gray)))
-                .listener(new RequestListener<String, GlideDrawable>() {
+                .apply(new RequestOptions().placeholder(new ColorDrawable(ContextCompat.getColor(mContext, android.R.color.darker_gray))))
+                .listener(new RequestListener<Drawable>() {
                     @Override
-                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
                         return false;
                     }
 
                     @Override
-                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
-                        /**
-                         * adjust the size of ImageView according to image
-                         */
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
                         if (imageView.centered) {
                             final RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(finalWidth, finalHeight);
                             params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
